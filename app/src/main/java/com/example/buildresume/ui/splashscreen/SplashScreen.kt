24bb6331e7b5d13e8fe0c77929525f2fb9450690
Pritesh Nikam <pritesh.nikam@coditas.com>
2022-10-19 16:22:46 +0530

    created splash screen

---
+++ app/src/main/java/com/example/buildresume/ui/splashscreen/SplashScreen.kt
@@ -0,0 +1,33 @@
+package com.example.buildresume.ui.splashscreen
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.buildresume.R
+
+class SplashScreen : Fragment() {
+
+    private var splashScreenDelay: Long = 2000
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            view.findNavController()
+                .navigate(SplashScreenDirections.actionSplashScreenToLoginScreen())
+        }, splashScreenDelay)
+
+    }
+}
